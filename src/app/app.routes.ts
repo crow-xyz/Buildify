9a94ef3345478ed64a83b0e7d689d983f6f63efe
ngarcia2023 <nic.garciab@duocuc.cl>
2024-06-13 14:00:34 -0400

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,19 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+  {
+    path: 'main',
+    loadComponent: () => import('./pages/main/main.page').then( m => m.MainPage)
   },
 ];
