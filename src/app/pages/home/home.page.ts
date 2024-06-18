720e52dd0c323bc77e6673108b3527395cf8553b
ngarcia2023 <nic.garciab@duocuc.cl>
2024-06-18 13:14:32 -0400

    add angular material

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonImg, IonButtons, IonButton, IonMenuButton, IonMenu, IonApp, IonItem, IonLabel, IonInput, IonCard, IonCardHeader } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, IonImg, IonButtons, IonButton, IonMenuButton, IonMenu, IonApp, IonItem, IonLabel, IonInput, IonCard, IonCardHeader]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonIcon, IonImg, IonButtons, IonButton, IonMenuButton, IonMenu, IonApp, IonItem, IonLabel, IonInput, IonCard, IonCardHeader, RouterLink]
 })
 export class HomePage implements OnInit {
 
