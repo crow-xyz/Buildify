4e1f2e3dc7025a874b27c1b0d240d4198fd49d1d
crow-xyz <nic.garciab@duocuc.cl>
2024-06-20 05:26:44 -0400

    agrega guards, mas fetchs y func principal(incompleta)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { introGuard } from './guards/intro.guard';
+import { authGuard } from './guards/auth.guard';
+import { autoLoginGuard } from './guards/auto-login.guard';
 
 export const routes: Routes = [
   {
@@ -8,11 +11,11 @@ export const routes: Routes = [
   },
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage),
   },
   {
     path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage),
   },
   {
     path: 'main',
@@ -22,5 +25,8 @@ export const routes: Routes = [
     path: 'register',
     loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
   },
-
+  {
+    path: 'intro',
+    loadComponent: () => import('./pages/intro/intro.page').then( m => m.IntroPage)
+  },
 ];
--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 import { HttpClient } from '@angular/common/http';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
@@ -12,24 +13,83 @@ import { HttpClient } from '@angular/common/http';
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelect, IonSelectOption]
 })
 export class MainPage implements OnInit {
-
-  data: any[] = [];
+  form: FormGroup;
+  cpuData: any[] = [];
+  gpuData: any[] = [];
+  ramData: any[] = [];
+  storageData: any[] = [];
+  motherboardData: any[] = [];
+  psuData: any[] = [];
+  caseData: any[] = [];
+  coolerData: any[] = [];
+  
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getData();
+    this.getCpu();
+    this.getGpu();
+    this.getMotherboard();
+    this.form = new FormGroup({
+      cpu: new FormControl(null, { validators: [Validators.required] }),
+      gpu: new FormControl(null, { validators: [Validators.required] }),
+      motherboard: new FormControl(null, { validators: [Validators.required] }),
+    });
+
+    this.form.valueChanges.subscribe(values => {
+      // Apply your logic to check the compatibility between the PC hardware components
+      // For example:
+      
+      if (values.cpu && values.gpu && values.motherboard) {
+        if (values.cpu !== 'compatibleCpu' || values.gpu !== 'compatibleGpu' || values.motherboard !== 'compatibleMotherboard') {
+          console.log('The components are not compatible');
+        } else {
+          console.log('The components are compatible');
+        }
+      }
+    });
+    
   }
 
-  getData() {
+  getCpu() {
     this.http.get<any>('http://127.0.0.1:8000/api/cpu/')
       .subscribe({
       next: (response) => {
-        this.data = response;
-        console.log('Data fetched');
+        this.cpuData = response;
+        console.log('Cpu data fetched');
       },
       error: (error) => {
         console.error('Error fetching data:', error);
       }
       });
+  }
+
+  getGpu() {
+    this.http.get<any>('http://127.0.0.1:8000/api/gpu/')
+      .subscribe({
+      next: (response) => {
+        this.gpuData = response;
+        console.log('Gpu data fetched');
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+      });
+  }
+  
+  getMotherboard() {
+    this.http.get<any>('http://127.0.0.1:8000/api/motherboard/')
+      .subscribe({
+      next: (response) => {
+        this.motherboardData = response;
+        console.log('Motherboard data fetched');
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+      });
+  }
+
+  checkCompatibility(cpuData) {
+
   }
 }
