817f6d7ea34b2fc5a0b271d0aacd8dcdfb9a4a21
crow-xyz <nic.garciab@duocuc.cl>
2024-06-19 22:13:23 -0400

    agrega api fetch

--- src/app/pages/main/main.page.ts
+++ src/app/pages/main/main.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
   styleUrls: ['./main.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonSelect, IonSelectOption]
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
+  data: any[] = [];
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.getData();
   }
 
+  getData() {
+    this.http.get<any>('http://127.0.0.1:8000/api/cpu/')
+      .subscribe({
+      next: (response) => {
+        this.data = response;
+        console.log('Data fetched');
+      },
+      error: (error) => {
+        console.error('Error fetching data:', error);
+      }
+      });
+  }
 }
